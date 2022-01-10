dbd2149e7d2a10408f82e1fbc9da3e70c6d529d1
Josh Shelton <joshshelton.sd@gmail.com>
2022-01-10 14:48:04 -0500

    added data, model, services, added refs and reloacted identitymodels to data layer

---
+++ MacroTrackerMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MacroTrackerMVC.Startup))]
+namespace MacroTrackerMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
