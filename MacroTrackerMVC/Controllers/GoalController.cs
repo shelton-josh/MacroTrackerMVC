5f037b1cd7cdbd3a30308ea6bd0c0b467d5cdc67
Josh Shelton <joshshelton.sd@gmail.com>
2022-01-25 09:22:55 -0500

    small tweaks to style/buttons and created goals service and controller for stretch goals

---
+++ MacroTrackerMVC/Controllers/GoalController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MacroTrackerMVC.Controllers
+{
+    public class GoalController : Controller
+    {
+        // GET: Daily
+        public ActionResult Index()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+
+
+
+            return View();
+        }
+
+        public ActionResult GetMyTime(DateTime myTime)
+        {
+
+
+            return View();
+        }
+    }
+}
